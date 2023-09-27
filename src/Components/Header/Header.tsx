f290a71a84da1454aa491d179595ff30ae1d3c05
Jose Cortez <jvazquez@nuxiba.com>
2023-09-27 03:58:08 -0600

    Feat(ContextApp): added context to read variables on other components

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { AppContext } from "../../Context/AppContext";
+import IconLogo from "../../assets/100ladrillos.png";
+import Button from "../Button/Button";
+import { FormProps } from "../../types";
+const Header = () => {
+	const { data } = useContext(AppContext);
+
+	return (
+		<header className="headerContainer">
+			<img className="logo" src={IconLogo} alt="Logo 100Ladrillos" />
+			<div className="buttons">
+				<Button onClick={() => {}}>Cómo funciona</Button>
+				{!data.touched ? (
+					<>
+						<Button type="outlined-rounded" onClick={() => {}}>
+							Entrar
+						</Button>
+						<Button type="contained-rounded" onClick={() => {}}>
+							Registrarme
+						</Button>
+					</>
+				) : (
+					<Button onClick={() => {}}>Entrar</Button>
+				)}
+			</div>
+		</header>
+	);
+};
+
+export default Header;
